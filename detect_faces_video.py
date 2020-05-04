7294e6e45db58d40b2836337fd0e2dfa9b56dc5a
Mayank Bajaj <41484766+mayank8200@users.noreply.github.com>
2020-05-04 13:41:43 +0530

    update

--- detect_faces_video.py
+++ detect_faces_video.py
@@ -1,5 +1,5 @@
 # USAGE
-# python detect_faces_video.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
+# python detect_faces_video.py 
 
 # import the necessary packages
 from imutils.video import VideoStream
